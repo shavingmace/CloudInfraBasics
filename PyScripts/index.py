5515c7a15a7a34328478e993a14a889bd3441927
shavingmace <pskyd0151@gamil.com>
2022-09-23 13:28:32 +0900

    점심 먹고 커밋

--- PyScripts/index.py
+++ PyScripts/index.py
@@ -3,14 +3,15 @@ import os
 print('-'*30)
 loc = "./docs"
 file_list = os.listdir(loc)
-print(loc, '의 문서 수는:', len(file_list))
+
 print('-'*30)
 
 ix= ['- [['+name[:-3]+']]\n' for name in file_list]
 
 print(ix)
     
-    
-with open('./result.txt', 'w') as file:
+with open('./PyScripts/result.txt', 'w') as file:
     file.writelines(ix)
 file.close()
+
+print(loc, '의 문서 수는:', len(file_list))
